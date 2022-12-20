5eb0bdd7d6ac0e2d9b1e89a0bbb9c66cb9f70400
Pasha <nikolenko.pavel@gmail.com>
2022-12-20 09:24:28 +0300

    Задание 1.

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,46 @@ public class Main {
         Car carKiaSportage = new Car();
         Car carHyundaiAvante = new Car();
 
+        carGranta.setBrand("Lada");
+        carGranta.setModel("Granta");
+        carGranta.setProductionYear(2015);
+        carGranta.setProductionCountry("Russia");
+        carGranta.setColor("yellow");
+        carGranta.setEngineVolume(1.7f);
+
+        carAudiA8.setBrand("Audi");
+        carAudiA8.setModel("A8 50 L TDI quattro");
+        carAudiA8.setProductionYear(2020);
+        carAudiA8.setProductionCountry("Germany");
+        carAudiA8.setColor("black");
+        carAudiA8.setEngineVolume(3.0f);
+
+        carBMWZ8.setBrand("BMW");
+        carBMWZ8.setModel("Z8");
+        carBMWZ8.setProductionYear(2021);
+        carBMWZ8.setProductionCountry("Germany");
+        carBMWZ8.setColor("black");
+        carBMWZ8.setEngineVolume(3.0f);
+
+        carKiaSportage.setBrand("Kia");
+        carKiaSportage.setModel("Sportage");
+        carKiaSportage.setProductionYear(2018);
+        carKiaSportage.setProductionCountry("South Korea");
+        carKiaSportage.setColor("red");
+        carKiaSportage.setEngineVolume(2.4f);
+
+        carHyundaiAvante.setBrand("Hyundai");
+        carHyundaiAvante.setModel("Avante");
+        carHyundaiAvante.setProductionYear(2016);
+        carHyundaiAvante.setProductionCountry("South Korea");
+        carHyundaiAvante.setColor("orange");
+        carHyundaiAvante.setEngineVolume(1.6f);
 
 
-
-        System.out.println("Hello world!");
+        System.out.println(carGranta);
+        System.out.println(carAudiA8);
+        System.out.println(carBMWZ8);
+        System.out.println(carKiaSportage);
+        System.out.println(carHyundaiAvante);
     }
 }
