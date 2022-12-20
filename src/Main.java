a1625509200233ace6b84cc6d31c7f552fa5da2c
Pasha <nikolenko.pavel@gmail.com>
2022-12-20 09:49:28 +0300

    Задание 3.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car carGranta = new Car("Lada", "Granta", 1.7f,
-                "yellow", 2015, "Russia");
+        Car carGranta = new Car("Lada", "", 1.7f,
+                "yellow", -2, "Russia");
         Car carAudiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0f,
-                "black", 2015, "Germany");
-        Car carBMWZ8 = new Car("BMW", "Z8", 3.0f, "black",
+                "", 2015, "Germany");
+        Car carBMWZ8 = new Car("", "Z8", 3.0f, "black",
                 2021, "Germany");
-        Car carKiaSportage = new Car("Kia", "Sportage", 2.4f,
-                "red", 2018, "South Korea");
+        Car carKiaSportage = new Car("Kia", "Sportage", 0,
+                "red", 2018, "");
         Car carHyundaiAvante = new Car("Hyundai", "Avante", 1.6f,
-                "orange", 2016, "South Korea");
+                "", 0, "South Korea");
 
 
         System.out.println(carGranta);
