b0e8135e4134266d8a528e22621adcebd533ea1c
Pasha <nikolenko.pavel@gmail.com>
2022-12-20 09:14:32 +0300

    Задание 1.

--- src/Car.java
+++ src/Car.java
@@ -4,5 +4,62 @@ public class Car {
     float engineVolume;
     String color;
     int productionYear;
-    int productionCountry;
+    String productionCountry;
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(float engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public void setProductionCountry(String productionCountry) {
+        this.productionCountry = productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Марка: " + brand +
+                ", модель: " + model +
+                ", объем двигателя: " + engineVolume +
+                ", цвет: " + color +
+                ", год производства: " + productionYear +
+                ", страна сборки: " + productionCountry;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
+        Car carGranta = new Car();
+        Car carAudiA8 = new Car();
+        Car carBMWZ8 = new Car();
+        Car carKiaSportage = new Car();
+        Car carHyundaiAvante = new Car();
+
+
+
+
         System.out.println("Hello world!");
     }
 }
